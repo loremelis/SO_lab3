c10f86cb3a69bc7b7f5c6e8e3d5a39f3223d4071
bob <100380168@alumnos.uc3m.es>
2018-04-25 10:20:37 +0200

    davide puzza

--- arcport.c
+++ arcport.c
@@ -10,13 +10,22 @@
 #define ATERRIZAJE 0
 #define DESPEGUE 1
 
+pthread_mutex_t mut_id;
+pthread_cond_t no_lleno;
+pthread_cond_t no_vacio;
+
+//int n_elementos;
+//sem_t elementos;
+//sem_t huecos;
+//int buffer[MAX_BUFFER];
+
+int disp_id = 0;
+
 
 void jefe_pista();
-//void radar();
-//void torre_de_control();
-
-pthread_mutex_t mut_id;
-int disp_id = 0;
+void radar();
+void torre_de_control();
+void print_banner();
 
 void print_banner()
 {
@@ -27,6 +36,34 @@ void print_banner()
 
 
 int main(int argc, char ** argv) {
+    
+    pthread_t th1, th2, th3;
+    
+    /*
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&no_lleno, NULL);
+    pthread_cond_init(&no_vacio, NULL);
+    */
+    
+    //sem_init(&elementos, 0, 0);
+    //sem_init(&huecos, 0, MAX_BUFFER);
+    
+    pthread_create(&th1, NULL, jefe_pista, NULL);
+    pthread_create(&th2, NULL, radar, NULL);
+    pthread_create(&th3, NULL, torre_de_control, NULL);
+    
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+    pthread_join(th3, NULL);
+    
+    //sem_destroy(&huecos);
+    //sem_destroy(&elementos);
+    
+    /*
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&no_lleno);
+    pthread_cond_destroy(&no_vacio);
+    */
     
     
 
