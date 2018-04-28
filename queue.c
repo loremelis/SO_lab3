88543db8d66c6fa7afe26c00ca9e26548c7b1243
nonci <davidem.cg@gmail.com>
2018-04-28 16:42:44 +0200

    bozza

--- queue.c
+++ queue.c
@@ -3,18 +3,23 @@
 #include "queue.h"
 #include <string.h>
 
-struct plane * buffer;
 
-/*To create a queue*/
+struct plane * buffer;
+int buffer_size = -1;
+int pos = 0, n_elementos;
+
+/* To create a queue of ptr to planes.
+ * Planes must be allocated from the caller */
 int queue_init(int size){
     buffer = malloc(sizeof(struct plane * )*size);
+	buffer_size = size;
     return 0;
 }
 
 /* To Enqueue an element*/
 int queue_put(struct plane* x) {
     pthread_mutex_lock(&mut_id);
-    while ()
+    while (n_elementos == buffer_size)
         pthread_cond_wait(&no_lleno, &mut_id);
     buffer[pos] = plane[i];
     pos = (pos + 1) % MAX_BUFFER;
